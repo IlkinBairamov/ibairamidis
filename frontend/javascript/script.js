dad33cb3a780073a10194b4984ee7696e2b4b280
IlkinBairamov <ilkinabr@code.edu.az>
2021-10-09 11:57:10 +0400

    frontend

---
+++ frontend/javascript/script.js
@@ -0,0 +1,173 @@
+
+
+
+// let names=['ilkin','nicat','ilyas'];
+
+// let address ={
+//     city:'baki',
+//     country:'azerbaijan'
+// }
+
+// var calculateAge= function(){
+// return 0;
+// };
+
+
+// console.log(typeof calculateAge);
+
+
+// let num1=Number('5');
+// let num2=Number('10');
+
+// // console.log(num1+num2)
+// // console.log(typeof num1 );
+
+
+// var name='ilkin';
+
+// var secondname='bayramov';
+// console.log(secondname)
+// var musteriTc='99743498564';
+// console.log(musteriTc)
+// var siparisT= 10;
+// console.log(siparisT)
+
+// var cins='erkek';
+// console.log(cins)
+// var address={
+//     city:'Baku',
+//     country:'Azerbaijan'
+// };
+// console.log(address)
+// var hobi=['cinema', 'game'];
+
+// console.log(hobi)
+
+
+
+// const yearOfBirth = 1986;
+
+// console.log(new Date().getFullYear()-yearOfBirth)
+
+// // Operatörler
+
+// let val;
+// const a=10;
+// const b=5;
+// const c=5;
+// let   d=3;
+
+// // 1- Aritmetik Operatörler
+
+// val = a+b;
+// val = a-b;
+// val = a*b;
+// val = a/b;
+// val = a%b;
+// val = d++;
+// val = ++d;
+// val = --d;
+// val = d--;
+
+// // 2- Atama Operatörleri
+
+// val = a;
+// val +=a; // val = val + a;
+// val -=a; // val = val - a;
+// val *=a; // val = val * a;
+// val /=a; // val = val / a;
+// val %=a; // val = val % a;
+
+
+// // 3- Karşılaştırma Operatörleri
+
+// val = a==b;
+// val = b==c;
+// val = b===c; // değer kontrolü & type
+// val = 5 === '5';
+// val = a!=b;
+// val = a!==b;
+// val = a > b;
+// val = b < a;
+// val = a >= b;
+// val = 5 >= 5;
+// val = a <= b;
+
+// // 4- Mantıksal Operatörler
+
+//    // && (And)
+//    // true  && true  => true
+//    // true  && false => false
+//    // false && false => false
+
+//    val=  (a>b) && (a>c) 
+//    val = (a>b) || (a<c)
+//    val = !(a>b)
+
+
+//    // || (Or)
+ 
+//    // true  && true  => true
+//    // true  && false => true
+//    // false && false => false
+
+//    // ! (Not)
+
+//    // !true  => false
+//    // !false => true
+
+// console.log(val);
+// console.log(typeof val);
+
+
+// let d = new Date();
+
+// let birthday = new Date(1965,5,22)
+
+// d.setFullYear(2020);
+// d.setHours(15)
+
+
+// console.log(d.getFullYear());
+// console.log(d.getDay());
+// console.log(d.getHours());
+// console.log(d.getDate()-birthday.getDay())
+// console.log(d.getDate()-birthday.getMonth())
+
+
+const fistName='Sena';
+const age = 19;
+const isStudent= true;
+const school='university';
+
+
+
+if(fistName==='Sena'){
+    console.log('merhaba Sena');
+}
+
+if(age===19){
+    console.log('yasiniz 19');
+}
+
+if(isStudent){
+    console.log('Mehraba ogrenci');
+}
+
+
+
+if(age>=18){
+    if(school== 'university'){
+        console.log('ehliyet alabilirsiniz');
+    }else(
+        console.log('ehliyet alamazsiniz')
+    )
+}
+
+
+
+
+
+
+
+
