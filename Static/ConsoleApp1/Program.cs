4cec4e04f6dbfdefa9cdf144eb2b05fe8863eeca
IlkinBairamov <ilkinabr@code.edu.az>
2021-10-14 21:44:27 +0400

    10/14/2021

---
+++ Static/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("name","surname",22);
+            Student s2 = new Student("name", "surname", 23);
+            Student s3 = new Student("name", "surname", 24);
+            Student s5 = new Student("name", "surname", 25);
+
+
+            Console.WriteLine(Student.CounterCalc());
+            Console.WriteLine(Student.Counter);
+
+        }
+    }
+}
---
+++ Static/ConsoleApp1/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        public readonly string Name;
+        public readonly string Surname;
+        public readonly int Age;
+        private static int Counter1;
+        public static int Counter
+        {
+            get
+            {
+                return Counter1;
+            }
+        }
+
+        public Student(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;           
+            Counter1++;
+        }
+
+
+        public static int CounterCalc()
+        {
+            return Counter1;
+        }
+            
+    }
+}
+
+
+
