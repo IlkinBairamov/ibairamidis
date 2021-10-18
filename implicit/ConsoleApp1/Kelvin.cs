95fd36f041ae75b6d50dcd4492fa867e65c03325
IlkinBairamov <ilkinabr@code.edu.az>
2021-10-18 21:41:27 +0400

    h

---
+++ implicit/ConsoleApp1/Kelvin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Kelvin
+    {
+        public double Degree { get; set; }
+
+        public Kelvin(double degree)
+        {
+            Degree = degree;
+        }
+
+        public static explicit operator Kelvin(Celsius celsius)
+        {
+            return new Kelvin(celsius.Degree + 273);
+        }
+
+    }
+}
---
+++ implicit/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kelvin k1 = new Kelvin(20);   
+            Celsius c1 = k1;
+
+            Celsius c2 = new Celsius(100);
+            Kelvin k2 = (Kelvin)c2;
+            
+            Console.WriteLine(c1.Degree);
+            Console.WriteLine(k2.Degree);
+
+
+        }
+    }
+}
