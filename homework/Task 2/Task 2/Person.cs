61b17875c9f30de985b6811a5a565b3141ae63e1
IlkinBairamov <ilkinabr@code.edu.az>
2021-10-21 15:52:18 +0400

    ho

---
+++ homework/Task 2/Task 2/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Person : ICompare
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Person temp = obj as Person;
+            if (temp != null)
+            {
+                if (this.Age > temp.Age)
+                    return 1;
+                if (this.Age < temp.Age)
+                    return -1;
+                else
+                    return 0;
+            }
+            else
+                throw new ArgumentException("Person deyil!");
+        }
+        public Person(string name, string surname, int age) : base()
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+    }
+}
