ba2e2423f0d7c33f7429b0b01f8183d15eba5ae9
IlkinBairamov <ilkinabr@code.edu.az>
2021-10-21 21:40:06 +0400

    task

---
+++ Arraylist,list/Arraylist,list/Mylist.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arraylist_list
+{
+    class MyList<T>:IEnumerable<T>
+    {
+        private T[] _items;
+
+        public MyList()
+        {
+            _items = new T[0];
+        }
+
+        public void AddItem(T value)
+        {
+            Array.Resize(ref _items, _items.Length + 1);
+            _items[_items.Length - 1] = value;
+        }
+
+        public void ShowItems()
+        {
+            foreach (T item in _items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void RemoveItem()
+        {
+            Array.Resize(ref _items, _items.Length - 1);
+        }
+
+        public void Uptadeitem(int index, T value)
+        {
+            _items[index] = value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in _items)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Arraylist,list/Arraylist,list/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Arraylist_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList arrayList = new ArrayList();
+            //arrayList.Add(1);
+            //arrayList.Add(5.6);
+            //arrayList.Add("sdfs");
+            //arrayList.Clear();
+            //arrayList.Add(10);
+            //arrayList.Remove(10);
+            //arrayList.RemoveAt(0);
+            //arrayList.RemoveRange(1, 2);
+            //foreach (var item in arrayList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //List<int> numbers = new List<int>();
+            MyList<int> members = new MyList<int>();
+            members.AddItem(1);
+            members.AddItem(1);
+            members.AddItem(1);
+            members.AddItem(1);
+            foreach (int item in members)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+}
